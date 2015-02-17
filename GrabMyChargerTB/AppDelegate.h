bccfc551c741c8112c0cd459b41f11328d54f407
tyronboyd <tyronboyd@gmail.com>
2015-02-17 22:47:15 +1100

    Initial Commit

---
+++ GrabMyChargerTB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrabMyChargerTB
+//
+//  Created by Tyron Boyd on 17/02/2015.
+//  Copyright (c) 2015 TyronPBoyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GrabMyChargerTB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GrabMyChargerTB
+//
+//  Created by Tyron Boyd on 17/02/2015.
+//  Copyright (c) 2015 TyronPBoyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
